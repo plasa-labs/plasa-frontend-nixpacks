b0b24461f00a27edcb9e3afa9bdd388ae307c436
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-01 02:49:44 -0300

    feat(home): replace home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import SpacesPage from '@/components/spaces/Spaces'
+import { Space } from '@/components/space/Space'
 
 export default function Home() {
-	return <SpacesPage />
+	return <Space spaceAddress={process.env.NEXT_PUBLIC_SPACE_ADDRESS as string} />
 }
