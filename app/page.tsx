b89bdb9cfc61fec34a72e573157a8d5fd6b68ec6
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-03 18:02:28 -0300

    fix(home): space

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Space } from '@/components/space/Space'
 
 export default function Home() {
-	return <Space spaceAddress={process.env.NEXT_PUBLIC_SPACE_ADDRESS as string} />
+	return <Space />
 }
