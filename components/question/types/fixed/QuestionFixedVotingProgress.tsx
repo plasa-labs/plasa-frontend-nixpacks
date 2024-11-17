3ed48ead71d9d95284a704064e0a6f4c7b35f11b
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-16 23:47:06 -0300

    feat(QuestionFixedVotingProgress): order options by points

--- components/question/types/fixed/QuestionFixedVotingProgress.tsx
+++ components/question/types/fixed/QuestionFixedVotingProgress.tsx
@@ -34,35 +34,38 @@ export default function QuestionVotingProgress() {
 				<CardTitle>Progreso de la Votación</CardTitle>
 			</CardHeader>
 			<CardContent>
-				{options.slice(1).map((option, index) => {
-					const percentage = totalPoints === 0
-						? 0
-						: (Number(option.data.pointsAtDeadline) / totalPoints) * 100
+				{options
+					.slice(1)
+					.sort((a, b) => Number(b.data.pointsAtDeadline) - Number(a.data.pointsAtDeadline))
+					.map((option, index) => {
+						const percentage = totalPoints === 0
+							? 0
+							: (Number(option.data.pointsAtDeadline) / totalPoints) * 100
 
-					return (
-						<div key={index + 1} className='mb-4 last:mb-0'>
-							<div className='flex justify-between mb-1'>
-								<span className='text-sm font-medium'>{option.data.title}</span>
-								<span className='text-sm font-medium'>{percentage.toFixed(1)}%</span>
-							</div>
-							<div className='relative pt-1'>
-								<div className='flex mb-2 items-center justify-between'>
-									<div>
-										<span className='text-lg font-semibold inline-block text-primary'>
-											{formatPoints(option.data.pointsAtDeadline)} puntos
-										</span>
-									</div>
-									<div className='text-right'>
-										<span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-primary text-primary-foreground'>
-											{option.data.voteCount.toString()} votos
-										</span>
-									</div>
+						return (
+							<div key={index + 1} className='mb-4 last:mb-0'>
+								<div className='flex justify-between mb-1'>
+									<span className='text-sm font-medium'>{option.data.title}</span>
+									<span className='text-sm font-medium'>{percentage.toFixed(1)}%</span>
 								</div>
-								<Progress value={percentage} className='h-2' />
+								<div className='relative pt-1'>
+									<div className='flex mb-2 items-center justify-between'>
+										<div>
+											<span className='text-lg font-semibold inline-block text-primary'>
+												{formatPoints(option.data.pointsAtDeadline)} puntos
+											</span>
+										</div>
+										<div className='text-right'>
+											<span className='text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-primary text-primary-foreground'>
+												{option.data.voteCount.toString()} votos
+											</span>
+										</div>
+									</div>
+									<Progress value={percentage} className='h-2' />
+								</div>
 							</div>
-						</div>
-					)
-				})}
+						)
+					})}
 			</CardContent>
 		</Card>
 	)
