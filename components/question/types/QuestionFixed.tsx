3d5e376f3bfd8603688834d38b4e49e894e8bc60
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-16 23:40:02 -0300

    feat: create QuestionFixed

---
+++ components/question/types/QuestionFixed.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import QuestionVotingOptions from '../QuestionVotingOptions'
+import QuestionVotingProgress from '../QuestionVotingProgress'
+
+export default function QuestionFixed() {
+	return (
+		<>
+			<QuestionVotingOptions />
+			<QuestionVotingProgress />
+		</>
+	)
+}
