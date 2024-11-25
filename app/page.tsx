4844f0f4704b6894e61f6b2ff47afeced6fb7292
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-24 21:53:45 -0300

    feat: create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-/**
- * @fileoverview Home page component - Root page of the application
- * displaying the main Space component.
- */
+'use client'
 
-import Space from '@/components/space/Space'
+import Home from '@/components/onboarding/Home'
 
-/**
- * Home page component that serves as the root route (/)
- * @returns {JSX.Element} The rendered Space component
- */
-export default function Home() {
-	return <Space />
+export default function HomePage() {
+	return <Home />
 }
