a92f5c3962eeda08d4fc65aab3b5bb62f2c61060
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-25 00:11:43 -0300

    feat(Profile): back to home button

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -2,6 +2,7 @@
 
 // React/Next.js imports
 import { type ReactElement } from 'react'
+import Link from 'next/link'
 
 // External dependencies
 import { usePrivy } from '@privy-io/react-auth'
@@ -17,7 +18,8 @@ import ProfileConnectionsCard from './ProfileConnectionsCard'
 import ProfileStampsCard from './ProfileStampsCard'
 import ProfileSkeletonLoader from './ProfileSkeletonLoader'
 import ProfilePointsCard from './ProfilePointsCard'
-
+import { Button } from '@/components/ui/button'
+import { ArrowLeft } from 'lucide-react'
 /**
  * Profile component handles user profile management including:
  * - Instagram connection
@@ -46,6 +48,13 @@ export default function Profile(): ReactElement {
 
 	return (
 		<div className="main-container px-4 sm:px-6">
+			<Link href='/' passHref prefetch={true}>
+				<Button variant='outline' className='mb-6'>
+					<ArrowLeft className='mr-2 h-4 w-4' />
+					{/* Volver a {space?.data.name ? space.data.name : 'Espacio'} */}
+					Volver a Inicio
+				</Button>
+			</Link>
 			<h1 className="text-3xl font-bold mb-6">Mi Perfil</h1>
 			<div className="grid gap-6 grid-cols-1 md:grid-cols-2">
 				<div>
