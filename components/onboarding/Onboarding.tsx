9f163149b9078bcedff9045c81589afdf9af55af
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-26 04:13:01 -0300

    fix(Onboarding): flex

--- components/onboarding/Onboarding.tsx
+++ components/onboarding/Onboarding.tsx
@@ -54,7 +54,7 @@ function OnboardingContent() {
 	}
 
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-background">
+		<div className="flex-1 flex items-center justify-center bg-background">
 			<div className="w-full max-w-2xl px-4">
 				<Card>
 					<CardHeader>
