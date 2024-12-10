73c4246b486ae3b36e4f95db7bfdd8774012c62a
nicoacosta.eth <nicoacosta8@gmail.com>
2024-12-10 17:04:16 -0300

    feat(Footer): remove links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 
 /**
  * Footer component that displays copyright information and social media links
@@ -7,19 +7,9 @@ import Link from 'next/link'
 export default function Footer() {
 	return (
 		<footer className="border-t border-border mt-auto">
-			<div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
-				<div className="flex flex-col md:flex-row justify-between items-center">
-					<div className="mb-4 md:mb-0">
-						<p className="text-sm">&copy; 2024 D&D. All rights reserved.</p>
-					</div>
-					<nav className="flex space-x-4">
-						<Link href="https://github.com/plasa-labs/" target="_blank" className="text-sm hover:underline">
-							GitHub
-						</Link>
-						{/* <Link href="https://t.me/nicoacosta8" target="_blank" className="text-sm hover:underline">
-							Telegram
-						</Link> */}
-					</nav>
+			<div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8 flex items-center justify-center">
+				<div className="text-center">
+					<p className="text-sm">&copy; 2024 D&D. All rights reserved.</p>
 				</div>
 			</div>
 		</footer>
